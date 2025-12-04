1d7db47f206ad8740b7ef9beb685c52a83275298
Youssef Tarek <ytelmalwany@gmail.com>
2025-12-03 23:43:51 -0800

    finised

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -3,12 +3,12 @@ import SectionTitle from "../SectionTitle";
 const data = [
   {
     id: 1,
-    icon: "fa fa-phone",
+
     contact: "+201025913655",
   },
   {
     id: 2,
-    icon: "fa fa-whatsapp",
+
     contact: "https://wa.me/+201025913655",
   },
 ];
@@ -23,12 +23,10 @@ const Contact = ({ styleSection }) => {
               key={item.id}
               className="mb-4 text-lg flex items-center gap-4 shadow-2xl p-4 rounded-md hover:bg-gray-100 transition-all hover:translate-y-[5px] duration-500  "
             >
-              <i className={item.icon}></i>
               {item.id === 2 ? (
                 <a
                   href={item.contact}
                   target="_blank"
-                  rel="noopener noreferrer"
                   className="text-blue-600 hover:underline"
                 >
                   Call me on WhatsApp
